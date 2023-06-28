bdc9363ad2d41738cf2e172ed0e698e7fd999e1e
pavanleton <pavankumarkpkp@gmail.com>
2023-06-28 23:35:14 +0530

    update auth layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+
+const Navbar = () => {
+    return <>
+
+        <nav
+            className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link href="#" className="flex items-center">
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+                        <span
+                            className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SupplyChain</span>
+                </Link>
+                <div className="flex md:order-2">
+                    <button type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
+                        started
+                    </button>
+                </div>
+            </div>
+        </nav>
+
+    </>
+}
+
+export default Navbar
