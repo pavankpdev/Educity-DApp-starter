ec0cbfe62144ccfe6269d08e28c5268ab136bd78
pavanleton <pavankumarkpkp@gmail.com>
2023-06-29 00:07:07 +0530

    add product cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
+import Product from "@/components/Product";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+        <div
+          className={'grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3'}
+        >
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+        </div>
     </main>
   )
 }
